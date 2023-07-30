b71c132ebad541b46b086388833e56821c5adaf2
MattHeffNT <heffa2004@gmail.com>
2023-07-30 23:13:32 +1000

    add port for listening

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+
+const port = process.env.PORT || 3000;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
